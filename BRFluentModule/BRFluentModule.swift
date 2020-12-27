fc5cf5a692ea683fce3d2d45cb91fe4797a9c87b
Shine <>
2020-12-27 23:20:34 +0800

    fix: change prefix to public

--- BRFluentModule/BRFluentModule.swift
+++ BRFluentModule/BRFluentModule.swift
@@ -9,21 +9,21 @@ import Foundation
 
 postfix operator ^+
 
-postfix func ^+ <K>(obj : K) -> BRFluentModule<K>{
+public postfix func ^+ <K>(obj : K) -> BRFluentModule<K>{
     return BRFluentModule(someObject: obj)
 }
 
 postfix operator ^-
 
-@discardableResult postfix func ^- <K>(obj : BRFluentModule<K>) -> K{
+@discardableResult public postfix func ^- <K>(obj : BRFluentModule<K>) -> K{
     return obj.someObject
 }
 
 @dynamicMemberLookup
-struct BRFluentModule<SomeObject>{
+public struct BRFluentModule<SomeObject>{
     var someObject : SomeObject
         
-    subscript<Value>( dynamicMember keyPath : WritableKeyPath<SomeObject,Value>)-> (Value)->(BRFluentModule<SomeObject>){
+    public subscript<Value>( dynamicMember keyPath : WritableKeyPath<SomeObject,Value>)-> (Value)->(BRFluentModule<SomeObject>){
         var tempObject = self.someObject
             
         return {value in
